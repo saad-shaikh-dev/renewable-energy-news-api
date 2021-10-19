34d82dc57b57c4fbd2574615368ded9e5ca1ba74
Saad Shaikh <saad.shaikh.dev@protonmail.com>
2021-10-19 15:55:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5006;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
