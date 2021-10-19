9dc5368668878ee94065f4e2f8054b740ea37f1f
Saad Shaikh <saad.shaikh.dev@protonmail.com>
2021-10-19 15:55:31 +0000

    Replace index.js

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 8000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const newspapers = [
+const newsSources = [
     {
         name: "telegraph",
-        address: "https://www.telegraph.co.uk/climate-change/",
+        domain: "https://www.telegraph.co.uk",
+        url: "https://www.telegraph.co.uk/renewable-energy/",
     },
     {
         name: "the-guardian",
-        address: "https://www.theguardian.com/environment/climate-crisis",
+        domain: "https://www.theguardian.com",
+        url: "https://www.theguardian.com/environment/renewableenergy",
     },
     {
         name: "the-times",
-        address: "https://www.thetimes.co.uk/environment/climate-change",
+        domain: "https://www.thetimes.co.uk",
+        url: "https://www.thetimes.co.uk/search?source=search-page&q=renewable",
     },
     {
         name: "al-jazeera",
-        address: "https://www.aljazeera.com/tag/climate-change/"
+        domain: "https://www.aljazeera.com",
+        url: "https://www.aljazeera.com/search/renewable"
     },
     {
-        name: "nytimes",
-        address: "https://www.nytimes.com/section/climate"
+        name: "recharge-news",
+        domain: "https://www.rechargenews.com",
+        url: "https://www.rechargenews.com/"
     },
     {
-        name: "cnn",
-        address: "https://edition.cnn.com/specials/world/cnn-climate"
+        name: "renewables-now",
+        domain: "https://renewablesnow.com/news",
+        url: "https://renewablesnow.com/"
     },
+    {
+        name: "science-daily",
+        domain: "https://www.sciencedaily.com",
+        url: "https://www.sciencedaily.com/search/?keyword=renewable#gsc.tab=0&gsc.q=renewable&gsc.page=1"
+    },
+    {
+        name: "renewable-energy-magazine",
+        domain: "https://www.renewableenergymagazine.com",
+        url: "https://www.renewableenergymagazine.com/search?cx=partner-pub-7794467828055047%3A8692188386&cof=FORID%3A9&ie=UTF-8&q=renewable"
+    },
+    {
+        name: "world-energy-news",
+        domain: "https://www.worldenergynews.com",
+        url: "https://www.worldenergynews.com/news/search?search=renewable"
+    }
 ];
 
 const app = express();
@@ -35,20 +56,21 @@ const app = express();
 const articles = [];
 
 app.get("/", (action, reaction) => {
-    reaction.json(`This is the climate change news API`);
+    reaction.json(`Hi, my name is Saad Shaikh and this is the climate change news API. Go to the link 'https://climate-change-feed-api.herokuapp.com/news' to get your news updates`);
 });
 
 app.get("/news", (action, reaction) => {
-    newspapers.map(newspaper =>
+    newsSources.map(newsSource =>
         axios
-            .get(newspaper.address)
+            .get(newsSource.url)
             .then(response => {
                 const html = response.data;
                 const $ = cheerio.load(html);
-                $('a:contains("climate")', html).each(function () {
+                $('a:contains("renewable")', html).each(function () {
                     const title = $(this).text();
                     const url = $(this).attr("href");
-                    articles.push({ title, url, source: newspaper.name });
+                    const link = () => url.startsWith("/") ? newsSource.domain + url : url
+                    articles.push({ title, url: link(), source: newsSource.name });
                 });
             })
             .catch(err => console.log(err))
@@ -56,25 +78,25 @@ app.get("/news", (action, reaction) => {
     reaction.json(articles);
 });
 
-app.get("/news/:newspaperId", (req, res) => {
-    const newspaperId = req.params.newspaperId;
-    const newspaperAddress = newspapers.filter(
-        newspaper => newspaper.name == newspaperId
-    )[0].address;
-    const newspaperBase = newspapers.filter(
-        newspaper => newspaper.name == newspaperId
-    )[0].base;
-
+app.get("/news/:newsSourceId", (req, res) => {
+    const newsSourceId = req.params.newsSourceId;
+    const newsSourceAddress = newsSources.filter(
+        newsSource => newsSource.name == newsSourceId
+    )[0].url;
+    /*   const newsSourceBase = newsSources.filter(
+           newsSource => newsSource.name == newsSourceId
+       )[0].base;*/
     axios
-        .get(newspaperAddress)
+        .get(newsSourceAddress)
         .then(response => {
             const html = response.data;
             const $ = cheerio.load(html);
             const specificArticles = [];
-            $('a:contains("climate")', html).each(function () {
+            $('a:contains("renewable")', html).each(function () {
                 const title = $(this).text();
                 const url = $(this).attr("href");
-                specificArticles.push({ title, url: newspaperBase + url, source: newspaperId });
+                /*   const link = () => url.startsWith("/") ? newsSourceAddress.domain + url : url */
+                specificArticles.push({ title, url, source: newsSourceId });
             });
             res.json(specificArticles)
 
